f27c857ec01f5002c3b0fd12ded5a59a6a78318e
Tom Howard <tom.howard@greater.com.au>
2024-07-19 16:16:20 +1000

    IE 11 compatibility changes

--- src/script.js
+++ src/script.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const parser = new UAParser();
-    const result = parser.getResult();
+    var parser = new UAParser();
+    var result = parser.getResult();
     
     // Update Browser Name in Heading
-    document.getElementById('browserName').textContent = `Your Browser: ${result.browser.name}`;
+    document.getElementById('browserName').textContent = 'Your Browser: ' + result.browser.name;
     
     // Fill Table with UAParser Details
-    const detailsTable = document.getElementById('uaDetails').getElementsByTagName('tbody')[0];
-    Object.keys(result).forEach(key => {
+    var detailsTable = document.getElementById('uaDetails').getElementsByTagName('tbody')[0];
+    Object.keys(result).forEach(function(key) {
         if (typeof result[key] === 'object') {
-            Object.keys(result[key]).forEach(subKey => {
-                const row = detailsTable.insertRow();
-                const cell1 = row.insertCell(0);
-                const cell2 = row.insertCell(1);
-                cell1.textContent = `${key.toUpperCase()} - ${subKey}`;
+            Object.keys(result[key]).forEach(function(subKey) {
+                var row = detailsTable.insertRow();
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                cell1.textContent = key.toUpperCase() + ' - ' + subKey;
                 cell2.textContent = result[key][subKey];
             });
         } else {
-            const row = detailsTable.insertRow();
-            const cell1 = row.insertCell(0);
-            const cell2 = row.insertCell(1);
+            var row = detailsTable.insertRow();
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
             cell1.textContent = key.toUpperCase();
             cell2.textContent = result[key];
         }
